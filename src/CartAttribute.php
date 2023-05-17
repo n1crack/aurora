bc7fe84464e084e0d2536d53f4cb589fcd75e163
Yusuf Özdemir <yusuf@ozdemir.be>
2023-05-17 13:30:33 +0300

    remove attribute validations

--- src/CartAttribute.php
+++ src/CartAttribute.php
@@ -12,17 +12,6 @@ class CartAttribute extends Collection
 
     public function __construct($items = [])
     {
-        $validator = Validator::make($items, [
-            'label' => ['required'],
-            'value' => ['required'],
-            'price' => ['sometimes', 'numeric'],
-            'weight' => ['sometimes', 'numeric'],
-        ]);
-
-        if ($validator->fails()) {
-            throw new \Exception($validator->errors()->first());
-        }
-
         parent::__construct($items);
     }
 }
