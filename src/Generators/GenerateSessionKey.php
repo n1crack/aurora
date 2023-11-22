338629b3d2903a9b3cc54b08f622d0cfef0c790c
Yusuf Özdemir <yusuf@ozdemir.be>
2023-11-22 20:23:09 +0300

    cleanup

--- src/Generators/GenerateSessionKey.php
+++ src/Generators/GenerateSessionKey.php
@@ -9,9 +9,6 @@ class GenerateSessionKey
 {
     public function __invoke(): string
     {
-        // todo: will be improved
-        // return Auth::check() ? 'user:' . Auth::id() : 'guest:' . uniqid();
-
         if (Auth::check()) {
             return 'user:' . Auth::id();
         }
